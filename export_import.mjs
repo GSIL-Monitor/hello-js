227e22c5c28e789c50a4d350191e7c2167e24d31
wentianlin <7989363@qq.com>
2018-12-09 21:56:33 +0800

    export-import

--- export_import.mjs
+++ export_import.mjs
@@ -1,9 +1,46 @@
-// jshint esversion:6
+// module_export
+// ES6模块设计尽量静态化，在编译阶段执行
+// 模块中顶层this为undefined，无意义
+
+let firstName = "wen";
+let lastName = "dabei";
+
+function sayName() {
+	console.log(`I'm ${firstName} ${lastName}`);
+}
+
+export {
+	firstName,
+	lastName,
+	sayName,
+	// 可以使用as更换名字
+	// sayName as myName,
+}
+
+// import时需要知道模块输出的变量名是什么，很不方便
+// 用户未必愿意阅读文档去了解模块有哪些属性和方法。
+// 用export default命令，为模块指定默认输出。
+// 一个模块只能有一个默认输出，因此export default命令只能使用一次
+// import命令后面不用加大括号，因为只可能唯一对应export default命令
+let obj = {
+	firstName,
+	lastName,
+	sayName,
+};
+export default obj;
+
+
+
+// import
 // import命令具有提升效果，会提升到整个模块的头部，首先执行
 // 如果想为输入的变量重新取一个名字，import命令要使用as关键字，将输入的变量重命名。
 // import是静态执行，所以不能使用表达式和变量，if语句等运行时才能得到结果的语法结构
 // 可以用as为变量更改名称
-import { firstName, lastName, sayName as myName } from "./module_export";
+import {
+	firstName,
+	lastName,
+	sayName as myName
+} from "./module_export";
 
 // 可以使用整体加载，即用星号（*）指定一个对象，所有输出值都加载在这个对象上面
 import * as myModule from "./module_export";
